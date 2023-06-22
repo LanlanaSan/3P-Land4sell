fc7f850cb70a0a1f7d78cea8157d1f9740bdc76b
LanlanaSan <lanlan.san@student.mahidol.ac.th>
2023-06-22 21:55:39 +0700

    fix firebase not define

--- Javascript/index.js
+++ Javascript/index.js
@@ -19,6 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+const functions = require('firebase-functions');
+
 firebase.initializeApp(functions.config().firebase);
 
 const app = initializeApp(firebaseConfig);
