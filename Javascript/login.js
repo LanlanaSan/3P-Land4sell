50eaacbd20e8b5979ebba597d0d1a808407db9b5
LanlanaSan <lanlan.san@student.mahidol.ac.th>
2023-07-09 01:53:04 +0700

    login with facebook

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, FacebookAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,12 +21,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics();
+const analytics = getAnalytics(app);
 const auth = getAuth();
+
+const provider = new FacebookAuthProvider();
+console.log(provider);
 
 var email = document.getElementById('email');
 var password = document.getElementById('password');
 
+//function for Email login
 window.login = function(e) {
     e.preventDefault();
     var obj = {
@@ -50,3 +54,32 @@ window.login = function(e) {
         })
     console.log(obj)
 }
+
+//function to login with Facebook
+document.getElementById("facebook-login").addEventListener("click", function() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // The signed-in user info.
+            const user = result.user;
+
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            const credential = FacebookAuthProvider.credentialFromResult(result);
+            const accessToken = credential.accessToken;
+
+            alert("Welcome " + user.displayName);
+            console.log(user);
+
+            window.location.replace("../after-login.html")
+        })
+        .catch((err) => {
+            const errorCode = err.code;
+            const errorMessage = err.message;
+            console.log(errorMessage);
+            // The email of the user's account used.
+            const email = err.customData.email;
+            // The AuthCredential type that was used.
+            const credential = FacebookAuthProvider.credentialFromError(err);
+        })
+})
+
+//function to login with Google
